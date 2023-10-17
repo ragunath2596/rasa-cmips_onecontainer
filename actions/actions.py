303137f88f87981b815cfa00ba66bfb7cbd50296
Vivek THakur <vivek.thakur.prof@gmail.com>
2023-10-17 09:44:24 +0530

    Revert "fix: responses ."
    This reverts commit 993736ba501adb6e56ac6d0b9395ddca3a947b60.

--- actions/actions.py
+++ actions/actions.py
@@ -128,6 +128,9 @@ class ValidateCallerVerificationForm(FormValidationAction):
                 }
             else:
                 logging.debug(f"Provider number verification failed")
+                dispatcher.utter_message(
+                    response="utter_invalid_provider_number"
+                )
                 save_database_mongo(tracker, "provider_number", "N")
                 return {
                     "slot_provider_number": None,
@@ -176,6 +179,9 @@ class ValidateCallerVerificationForm(FormValidationAction):
                 }
             else:
                 logging.debug(f"provider_name verification failed")
+                dispatcher.utter_message(
+                    response="utter_invalid_full_name"
+                )
                 save_database_mongo(tracker, "provider_name", "N")
                 return {
                     "slot_provider_name": None,
@@ -233,6 +239,9 @@ class ValidateCallerVerificationForm(FormValidationAction):
                 else:
                     logging.debug(f"provider_ssn verification failed")
                     num_string = [" ".join(num) for num in str(number_)]
+                    dispatcher.utter_message(
+                    response="utter_invalid_SSN"
+                    )
                     return {
                         "slot_provider_ssn": None,
                         "slot_provider_number_failure_count": slot_provider_number_failure_count + 1,
@@ -280,6 +289,7 @@ class ValidateCallerVerificationForm(FormValidationAction):
                 }
             else:
                 logging.debug(f"provider_county verification failed")
+                dispatcher.utter_message(response="utter_invalid_county")
                 save_database_mongo(tracker, "provider_county", "N")
                 return {
                     "slot_provider_county": None,
