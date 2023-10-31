0c37a1c648d0f360bbc3dae234dd7d45e6e8962a
ragunath2596 <144646312+ragunath2596@users.noreply.github.com>
2023-10-31 14:08:55 +0530

    Update cmips_nlu.sh

--- cmips_nlu.sh
+++ cmips_nlu.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-rasa run --enable-api --cors "*" --port 5005 --debug  --log-file nlu-debug.log
+# Start the Rasa actions server on port 5055
+rasa run actions --port 5055 --debug &
+
+# Start the Rasa NLU server on port 5005
+rasa run --enable-api --cors "*" --port 5005 --debug --log-file nlu-debug.log
